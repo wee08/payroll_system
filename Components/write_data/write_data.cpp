76b2a18283117e46245f3c277f3798027a94188a
wee08 <manutnithyasean@gmail.com>
2026-06-05 16:09:40 +0700

    Remove unused files and implement initial CSV writing functionality

--- Components/write_data/write_data.cpp
+++ Components/write_data/write_data.cpp
@@ -1,4 +1,13 @@
 #include "./Header/read_from_file/read_CSV.h"
+#include "./Header/write_to_file/write_to_file.h"
+#include "./Header/structure.h"
+#include "../utils/calculate_attendance.h"
+
 int main(){
-    read_CSV("../../Data/attendance.csv");
+    string file_path = "../../Data/attendance.csv";
+    // write_CSV(file_path);
+    // write_CSV("../../Data/attendance.csv");
+    // Employee * e = read_CSV("../../Data/attendance.csv");
+    Employee * e = read_CSV(file_path);
+    calculate_attendance(e,file_path);
 }
