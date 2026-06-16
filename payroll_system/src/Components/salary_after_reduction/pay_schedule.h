ecb0331fd1c3ed5cfac82b0c9ccdf21c3fdbc49e
wee08 <manutnithyasean@gmail.com>
2026-06-16 19:45:39 +0700

    implement space display in terminal

--- payroll_system/src/Components/salary_after_reduction/pay_schedule.h
+++ payroll_system/src/Components/salary_after_reduction/pay_schedule.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 #include "../../Model/structure.h"
 #include "../IO/read_CSV.h"
@@ -25,7 +26,11 @@ void display(Node* head) {
     double salary;
     Node* temp = head;
     Attendance_record * ar = att_l->head;
-    cout << "Name, Pay Date, Payment\n";
+    cout << endl << 
+        left << setw(20) << "Name" 
+        << setw(12) << "Payment" 
+        << "Pay Date" << endl
+        << string(47,'-') << endl;
     
     ifstream file_out;
     file_out.open(final_salary_file);
@@ -38,18 +43,16 @@ void display(Node* head) {
     while(getline(file_out,line)) {
         salary = stod(line);
         total += salary;
+        
 
-        cout << salary << endl;
+        cout<< left 
+            << setw(20) <<ar->name
+            << setw(12) << salary
+            << "[ 12-Jun-2026 ]" << endl;
 
-        // double salary = temp->data.workingDay * temp->data.pay;
-
-        // cout << temp->data.name << "\t"
-        //      << temp->data.workingDay << "\t"
-        //      << salary << endl;
-
-        // total += salary;
+        ar = ar->next;
     }
-
+    cout << endl;
     cout << "Total = $" << total << endl;
 }
 
